7f6763bef87b1402022e60b14a86d4449f8790e2
Oleh Yemelianov <emelianov2811@gmail.com>
2025-01-30 01:17:00 +0200

    Requests updated

--- discordbot/main.py
+++ discordbot/main.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 from discord.utils import get
 import youtube_dl
 import pafy
+import asyncio
+import os
 from request_api import find_anime
 
 import os
@@ -18,7 +20,7 @@ bot = commands.Bot(command_prefix='!',intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Вітаю,{bot.user}!')
+    print(f'{bot.user} успішно під`єднано!')
     
     
 @bot.event
@@ -32,33 +34,26 @@ async def on_message(message):
          
 @bot.command()
 async def ping(ctx):
-    await ctx.send(f'{ctx.author.name}, ти шукав мене?')
+    await ctx.send(f'{ctx.author.name}, Hello!')
     
-@bot.command()
-async def play(ctx, url:str):
-    if not ctx.author.voice:
-        return await ctx.send('Ви не під`єднані до каналу!')
-    voice_channel = ctx.author.voice.channel
-    if ctx.voice_client is None:
-        vc = await voice_channel.connect
-    else:
-        await ctx.voice_client.move_to(voice_channel)
-        vc = ctx.voice_client
-    
-    
-    video = pafy.new(url)
-    best = video.getbestaudio
-    source = await discord.FFmpegOpusAudio.from_probe(best.url, method='fallback')
-    
-    vc.play(source)
-    
-    
-@bot.command()
-async def leave(ctx):
-    if ctx.voice_client:
-        await ctx.guild.voice_client.disconnect()
-    else:
-        await ctx.send('Бот не під`єднаний до голосового каналу.')  
+async def get_audio_url(url): 
+    loop = asyncio.get_event_loop() 
+    data = await loop.run_in_executor(None, lambda: os.popen(f"youtube-dl -f bestaudio --get-url {url}").read()) 
+    return data.strip() 
+
+@bot.command() 
+async def play(ctx, url: str): 
+    voice_channel = ctx.author.voice.channel 
+    audio_url = await get_audio_url(url) 
+
+    vc = await voice_channel.connect() 
+
+    vc.play(discord.FFmpegPCMAudio(audio_url)) 
+
+    while vc.is_playing(): 
+        await asyncio.sleep(1) 
+
+    await vc.disconnect() 
         
 
 @bot.command(name='anime')
