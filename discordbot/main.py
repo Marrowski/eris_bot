64f872dfaa03fdc3cdee127ea4f0ca949480608c
Oleh Yemelianov <emelianov2811@gmail.com>
2025-01-31 21:25:38 +0200

    Added work with database

--- discordbot/main.py
+++ discordbot/main.py
@@ -1,11 +1,15 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
-import youtube_dl
-import pafy
+
+import random
+import yt_dlp
 import asyncio
+
 import os
+
 from request_api import find_anime
+from database import insert_data
 
 import os
 from dotenv import load_dotenv
@@ -25,11 +29,15 @@ async def on_ready():
     
 @bot.event
 async def on_message(message):
-    print(f'Отримано повідомлення! Текст:{message.content}, Сервер:{message.guild}')
+    user = message.author.name
+    guild = message.guild.name
+    msg = message.content
+    await insert_data(user, guild, msg)
+    await bot.process_commands(message)
+    print(f'Отримано повідомлення! Текст:{msg}, Сервер:{guild}')
     
     if message.author.bot:
         return
-    await bot.process_commands(message)
         
          
 @bot.command()
@@ -37,28 +45,74 @@ async def ping(ctx):
     await ctx.send(f'{ctx.author.name}, Hello!')
     
 async def get_audio_url(url): 
-    loop = asyncio.get_event_loop() 
-    data = await loop.run_in_executor(None, lambda: os.popen(f"youtube-dl -f bestaudio --get-url {url}").read()) 
-    return data.strip() 
+    loop = asyncio.get_running_loop()
+    
+    def fetch_url():
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'quiet': True,
+            'noplaylist': True
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+            return info.get('url', '').strip()
+    
+    return await loop.run_in_executor(None, fetch_url)
+    
 
-@bot.command() 
-async def play(ctx, url: str): 
-    voice_channel = ctx.author.voice.channel 
-    audio_url = await get_audio_url(url) 
+@bot.command()
+async def play(ctx, url: str):
+    global voice_channel
+    if ctx.author.voice and ctx.author.voice.channel:
+        voice_channel = ctx.author.voice.channel
+        audio_url = await get_audio_url(url)
 
-    vc = await voice_channel.connect() 
+        vc = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+        if not vc or not vc.is_connected():
+            vc = await voice_channel.connect()
+        else:
+            await vc.move_to(voice_channel)
 
-    vc.play(discord.FFmpegPCMAudio(audio_url)) 
+        await ctx.send(f'🎶 Connecting to {voice_channel.name}...')
+        
+        ffmpeg_options = {
+            'options': '-vn'
+        }
+        vc.play(discord.FFmpegPCMAudio(audio_url, **ffmpeg_options))
+        await ctx.send(f'Your media is now playing in {voice_channel.name}.')
 
-    while vc.is_playing(): 
-        await asyncio.sleep(1) 
-
-    await vc.disconnect() 
+        while vc.is_playing():
+            await asyncio.sleep(1)
+    else:
+        await ctx.send(f'❌ {ctx.author.mention}, connect to a voice channel first!')
+        
+        
+@bot.command()
+async def stop(ctx):
+    vc = ctx.voice_client
+    if vc:
+        await vc.disconnect()
+        await ctx.send(f'Bot has been disconnected from the channel: {voice_channel}')
+    else:
+        await ctx.send('Bot is not connected to voice channel.')
+        
+@bot.command()
+async def dice(ctx):
+    await ctx.send('Let`s roll the dice...')
+    await asyncio.sleep(2)
+    await ctx.send(f'Your number is: {random.randrange(0, 6)}!')
+    
         
 
 @bot.command(name='anime')
 async def anime_command(ctx, title:str):
     await find_anime(ctx, title)
+
+async def info(ctx):
+    user = ctx.author
+    guild = ctx.guild
+    msg = ctx.message.content
+    return user, guild, msg
 
 if __name__ == '__main__':
     bot.run(bot_tok)
